cf7426a75495c2867b2d66952b3a38462b322592
BAS1D <88146553+BAS1D@users.noreply.github.com>
2021-07-31 16:49:35 +0530

    Add files via upload

---
+++ Day3/Assignment3.py
@@ -0,0 +1,10 @@
+#Assignment3
+P = [2, 1, 5, 3, 4]
+print("gift presented to", P)
+P.remove(4)
+P.remove(5)
+P.insert(2,4)
+P.insert(3,5)
+print("gift recieved from",P)
+
+
---
+++ Day3/Assignment4 Final project.py
@@ -0,0 +1,4 @@
+#Assignment4
+x = float(input('Enter temperature in Celsius:'))
+y = x*(9/5)+32
+print("Temperature in Farenheit:", y)
